1cb76cf86f7ce71faa52683c8a32e27f9d1adc7b
Jermaine Antwi <jermaineantwi22@gmail.com>
2020-07-24 17:02:22 +0000

    gave behaviour to navbar

--- js/index.js
+++ js/index.js
@@ -1,22 +1,7 @@
- /* Swiperjs */
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-})
+});
