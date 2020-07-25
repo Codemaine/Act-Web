a3249593fbb8f97dc3ab126e6fa311e15655e33b
Jermaine Antwi <jermaineantwi22@gmail.com>
2020-07-25 17:22:41 +0000

    gave 'Responsive Website' a payment method

--- js/index.js
+++ js/index.js
@@ -5,3 +5,24 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+// Responsive Website Payment
+function webd() {
+
+var paylike = Paylike('89e97b47-96c3-4e81-83de-fecf7c4399b3');
+
+	paylike.popup({
+    title: 'Buy Responsive Website',
+    description: 'Email jermaineantwi22@gmail.com and add the images and the look of the website.',
+		currency: 'USD',
+		amount: 8000,
+	}, function( err, res ){
+			if (err)
+				return console.log(err);
+
+		console.log(res.transaction.id);
+
+		alert('Thank you!');
+	});
+
+}
